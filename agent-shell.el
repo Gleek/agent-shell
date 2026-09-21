164332bcd459a23a6aaf7b4587905cef56aaff05
xenodium <8107219+xenodium@users.noreply.github.com>
2026-09-21 15:20:22 +0100

    Bumping version

--- agent-shell.el
+++ agent-shell.el
@@ -4,10 +4,10 @@
 
 ;; Author: Alvaro Ramirez https://xenodium.com
 ;; URL: https://github.com/xenodium/agent-shell
-;; Version: 0.78.1
+;; Version: 0.78.2
 ;; Package-Requires: ((emacs "29.1") (shell-maker "0.97.3") (acp "0.15.1"))
 
-(defconst agent-shell--version "0.78.1")
+(defconst agent-shell--version "0.78.2")
 
 ;; Minimum dependency versions, as declared in the `Package-Requires'
 ;; header above.  Package managers that resolve versions enforce the
